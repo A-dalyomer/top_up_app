7df5a18382a34df1a49698c2e0200c1c00830cbd
ahmad daly <Ahmaddo1999@gmail.com>
2024-08-27 20:17:35 +0400

    Imported new localization files

--- lib/src/feature/localization/domain/util/app_localizations.dart
+++ lib/src/feature/localization/domain/util/app_localizations.dart
@@ -4,7 +4,10 @@ class AppLocalizations {
   static String appName = "App_name";
   static String homeScreen = "home_screen";
   static String loginScreen = "login_screen";
-  static String enterPassword = "enter_password";
+  static String loginScreenTitle = "login_screen_title";
+  static String phoneNumberLabel = "phone_number_label";
+  static String passwordLabel = "password_label";
+  static String passwordHint = "password_hint";
   static String shortPassword = "short_password";
   static String loginButton = "login_button";
   static String userBalance = "user_balance";
@@ -15,4 +18,8 @@ class AppLocalizations {
   static String mobileRecharge = "mobile_recharge";
   static String history = "history";
   static String recharge = "recharge";
+  static String beneficiaryName = "beneficiary_name";
+  static String save = "save";
+  static String add = "add";
+  static String addBalance = "add_balance";
 }
