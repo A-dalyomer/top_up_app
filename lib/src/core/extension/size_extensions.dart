ae81967a360fd185f9fd6d4ef0dc33cb444d05a3
ahmad daly <Ahmaddo1999@gmail.com>
2024-08-28 20:42:59 +0400

    Added new size extensions

--- lib/src/core/extension/size_extensions.dart
+++ lib/src/core/extension/size_extensions.dart
@@ -3,4 +3,9 @@ import 'package:flutter/material.dart';
 extension SizeExtensions on num {
   Padding get vS => Padding(padding: EdgeInsets.only(bottom: toDouble()));
   Padding get hS => Padding(padding: EdgeInsets.only(left: toDouble()));
+
+  double height(BuildContext context) =>
+      MediaQuery.of(context).size.height * this;
+  double width(BuildContext context) =>
+      MediaQuery.of(context).size.width * this;
 }
