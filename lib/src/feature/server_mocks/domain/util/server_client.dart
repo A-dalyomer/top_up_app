984c59ab34f648bfc76a1cb501a322a549d9b798
ahmad daly <Ahmaddo1999@gmail.com>
2024-08-28 23:21:05 +0400

    Mock server client add beneficiary support

--- lib/src/feature/server_mocks/domain/util/server_client.dart
+++ lib/src/feature/server_mocks/domain/util/server_client.dart
@@ -8,50 +8,60 @@ import 'package:uae_top_up/src/feature/configurarion/data/app_config_model.dart'
 import 'package:uae_top_up/src/feature/configurarion/domain/entity/app_config.dart';
 import 'package:uae_top_up/src/feature/local_storage/data/constants/const_storage_keys.dart';
 import 'package:uae_top_up/src/feature/local_storage/domain/repository/local_storage_repository.dart';
+import 'package:uae_top_up/src/feature/user_management/data/model/beneficiary_model.dart';
 import 'package:uae_top_up/src/feature/user_management/data/model/user_model.dart';
 
 import '../../../network/data/constants/const_api_paths.dart';
 
 class ServerClient {
   MockClient mockClient() {
-    return MockClient(
-      (request) async {
-        if (kDebugMode) {
-          print(request.url.path);
-          print(request.headers);
-          print(request.body);
+    return MockClient(handler);
+  }
+
+  Future<http.Response> handler(request) async {
+    if (kDebugMode) {
+      print(request.url.path);
+      print(request.headers);
+      print(request.body);
+    }
+    switch (request.url.path) {
+      case ConstApiPaths.getAppConfig:
+        return appConfigResponse();
+      case ConstApiPaths.login:
+        final String? phoneNumber = jsonDecode(request.body)['phone_number'];
+        if (phoneNumber == null || phoneNumber.length < 8) {
+          return http.Response(
+            jsonEncode({"message": 'Missing phone number'}),
+            404,
+          );
         }
-        switch (request.url.path) {
-          case ConstApiPaths.getAppConfig:
-            return appConfigResponse();
-          case ConstApiPaths.login:
-            final String? phoneNumber =
-                jsonDecode(request.body)['phone_number'];
-            if (phoneNumber == null || phoneNumber.length < 8) {
-              return http.Response(
-                jsonEncode({"message": 'Missing phone number'}),
-                404,
-              );
-            }
-            final savedUsers = await getSavedUsersList();
-            final List<UserModel> matchingUsers = savedUsers
-                .where((element) => element.phoneNumber == phoneNumber)
-                .toList();
-            late final UserModel resultUser;
-            if (matchingUsers.isEmpty) {
-              resultUser = await createUser(phoneNumber, savedUsers);
-            } else {
-              resultUser = matchingUsers[0];
-            }
-            return http.Response(
-              jsonEncode(resultUser.toJson()),
-              200,
-            );
-          default:
-            return http.Response(jsonEncode({"message": 'Not Found'}), 404);
+        final savedUsers = await getSavedUsersList();
+        final List<UserModel> matchingUsers = savedUsers
+            .where((element) => element.phoneNumber == phoneNumber)
+            .toList();
+        late final UserModel resultUser;
+        if (matchingUsers.isEmpty) {
+          resultUser = await createUser(phoneNumber, savedUsers);
+        } else {
+          resultUser = matchingUsers[0];
         }
-      },
-    );
+        return http.Response(
+          jsonEncode(resultUser.toJson()),
+          200,
+        );
+      case ConstApiPaths.addBeneficiary:
+        final String? senderPhoneNumber =
+            jsonDecode(request.body)['sender_phone_number'];
+        final String? phoneNumber = jsonDecode(request.body)['phone_number'];
+        final String? beneficiaryName = jsonDecode(request.body)['name'];
+        return await addBeneficiaryResponse(
+          senderPhoneNumber: senderPhoneNumber!,
+          phoneNumber: phoneNumber!,
+          beneficiaryName: beneficiaryName!,
+        );
+      default:
+        return http.Response(jsonEncode({"message": 'Not Found'}), 404);
+    }
   }
 
   http.Response appConfigResponse() {
@@ -70,12 +80,40 @@ class ServerClient {
     );
   }
 
+  Future<http.Response> addBeneficiaryResponse({
+    required String senderPhoneNumber,
+    required String phoneNumber,
+    required String beneficiaryName,
+  }) async {
+    final savedUsers = await getSavedUsersList();
+    final senderUser = savedUsers.singleWhere(
+      (element) => element.phoneNumber == senderPhoneNumber,
+    );
+    BeneficiaryModel newBeneficiary = BeneficiaryModel(
+      name: beneficiaryName,
+      phoneNumber: phoneNumber,
+      transactions: const [],
+      id: senderUser.beneficiaries.length,
+    );
+    if (senderUser.beneficiaries.contains(newBeneficiary)) {
+      return http.Response(
+        jsonEncode({"message": "Already exists"}),
+        409,
+      );
+    }
+    senderUser.beneficiaries.add(newBeneficiary);
+    await saveUsers(savedUsers);
+    return http.Response(
+      jsonEncode(newBeneficiary.toJson()),
+      200,
+    );
+  }
+
   Future<List<UserModel>> getSavedUsersList() async {
     String? savedUsersRaw = (await DIManager.getIt<LocalStorageRepository>()
             .readValue(ConstStorageKeys.serverUsers)) ??
         "{}";
-    final List<Map<String, dynamic>> usersJson =
-        jsonDecode(savedUsersRaw)['users'] ?? [];
+    final List usersJson = jsonDecode(savedUsersRaw)['users'] ?? [];
     final savedUsers =
         usersJson.map((userData) => UserModel.fromJson(userData)).toList();
     return savedUsers;
@@ -94,12 +132,16 @@ class ServerClient {
       transactions: [],
     );
     savedUsers.add(resultUser);
+    await saveUsers(savedUsers);
+    return resultUser;
+  }
+
+  Future<void> saveUsers(List<UserModel> savedUsers) async {
     await DIManager.getIt<LocalStorageRepository>().writeValue(
       ConstStorageKeys.serverUsers,
       jsonEncode(
         {"users": savedUsers.map((e) => e.toJson()).toList()},
       ),
     );
-    return resultUser;
   }
 }
