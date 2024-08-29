d4d55f3b9c46f8607d0a32f4a885babe716351f0
ahmad daly <Ahmaddo1999@gmail.com>
2024-08-29 16:10:53 +0400

    Fixed empty transaction history text translation

--- lib/src/feature/transaction/presentation/widget/empty_transaction_history.dart
+++ lib/src/feature/transaction/presentation/widget/empty_transaction_history.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:uae_top_up/src/core/extension/size_extensions.dart';
 import 'package:uae_top_up/src/feature/localization/domain/util/app_localizations.dart';
@@ -13,7 +14,7 @@ class EmptyTransactionHistory extends StatelessWidget {
         const Icon(Icons.history, size: 60),
         16.vS,
         Text(
-          AppLocalizations.transactionHistoryEmpty,
+          AppLocalizations.transactionHistoryEmpty.tr(),
           style: Theme.of(context).textTheme.titleMedium,
           textAlign: TextAlign.center,
         )
