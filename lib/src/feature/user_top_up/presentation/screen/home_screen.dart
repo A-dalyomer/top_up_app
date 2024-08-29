82573744cc8e9ec6c8f6dd6542cdc5211abf02c7
ahmad daly <Ahmaddo1999@gmail.com>
2024-08-29 04:31:07 +0400

    Home screen refactor: new widget "balanceView"

--- lib/src/feature/user_top_up/presentation/screen/home_screen.dart
+++ lib/src/feature/user_top_up/presentation/screen/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:uae_top_up/src/core/widget/action_button.dart';
 import 'package:uae_top_up/src/feature/localization/domain/util/app_localizations.dart';
 import 'package:uae_top_up/src/feature/user_management/domain/util/user_actions.dart';
 import 'package:uae_top_up/src/feature/user_management/presentation/provider/user_management_provider.dart';
+import 'package:uae_top_up/src/feature/user_top_up/presentation/widget/balance_view.dart';
 import 'package:uae_top_up/src/feature/user_top_up/presentation/widget/beneficiaries_list_view.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -47,22 +48,7 @@ class HomeScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Selector<UserManagementProvider, double>(
-                  selector: (context, provider) => provider.user.balance,
-                  builder: (context, userBalance, child) => Text.rich(
-                    TextSpan(children: [
-                      TextSpan(
-                        text: '${AppLocalizations.userBalance.tr()}: ',
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                      TextSpan(
-                        text: userBalance.toStringAsFixed(0),
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                    ]),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
+                const BalanceView(),
                 const BeneficiariesListView(),
                 ActionButton(
                   title: AppLocalizations.add.tr(),
