d7e000df4101689f48a2c48d17c120581f7cd4d2
ahmad daly <Ahmaddo1999@gmail.com>
2024-08-30 21:06:26 +0400

    Create login form widget keys to be used in tests

---
+++ lib/src/core/constants/const_widget_keys.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ConstWidgetKeys {
+  static const passwordFormField = Key("PasswordTextFormFieldKey");
+  static const phoneFormField = Key("PhoneFormFieldKey");
+}
