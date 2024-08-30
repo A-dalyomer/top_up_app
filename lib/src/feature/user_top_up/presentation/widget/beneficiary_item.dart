48aaa1213ded39496d365629b84f802125a4d419
ahmad daly <Ahmaddo1999@gmail.com>
2024-08-31 02:56:26 +0400

    Beneficiary item removed unused inkwell widget

--- lib/src/feature/user_top_up/presentation/widget/beneficiary_item.dart
+++ lib/src/feature/user_top_up/presentation/widget/beneficiary_item.dart
@@ -27,35 +27,31 @@ class BeneficiaryItem extends StatelessWidget {
         ],
       ),
       width: MediaQuery.of(context).size.width * 0.36,
-      child: InkWell(
-        borderRadius: BorderRadius.circular(8),
-        onTap: () {},
-        child: Padding(
-          padding: const EdgeInsets.all(6.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                beneficiary.name,
-                style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                maxLines: 2,
-                textAlign: TextAlign.center,
-                overflow: TextOverflow.ellipsis,
+      child: Padding(
+        padding: const EdgeInsets.all(6.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              beneficiary.name,
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+            ),
+            FittedBox(
+              child: Text(
+                beneficiary.phoneNumber,
+                style: Theme.of(context).textTheme.bodyMedium,
               ),
-              FittedBox(
-                child: Text(
-                  beneficiary.phoneNumber,
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ),
-              ActionButton(
-                title: AppLocalizations.rechargeNow.tr(),
-                onPressed: onButtonTap,
-              )
-            ],
-          ),
+            ),
+            ActionButton(
+              title: AppLocalizations.rechargeNow.tr(),
+              onPressed: onButtonTap,
+            )
+          ],
         ),
       ),
     );
