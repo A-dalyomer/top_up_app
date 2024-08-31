f801977c9ab75d41af9f102c22d7488a321227aa
ahmad daly <Ahmaddo1999@gmail.com>
2024-09-01 00:50:57 +0400

    Settings dialog refactors for tests, Removed the settings consumer from top level widget since it was crashing the test for no reason, Added test mode to remove usage of context.locale in tests

--- lib/src/feature/configuration/presentation/widget/settings_dialog.dart
+++ lib/src/feature/configuration/presentation/widget/settings_dialog.dart
@@ -10,7 +10,20 @@ import '../../../user_management/presentation/provider/user_management_provider.
 import '../../../configuration/presentation/widget/settings_item.dart';
 
 class SettingsDialog extends StatelessWidget {
-  const SettingsDialog({super.key});
+  const SettingsDialog({super.key, this.testMode = false});
+  final bool testMode;
+
+  void changeLanguage(BuildContext context, Locale locale) {
+    context.setLocale(locale);
+  }
+
+  void signOut(BuildContext context) {
+    Navigator.pop(context);
+    context.read<UserManagementProvider>().signOutUser();
+  }
+
+  toggleTheme(SettingsProvider settingsProvider) =>
+      settingsProvider.toggleDarkMode();
 
   @override
   Widget build(BuildContext context) {
@@ -26,65 +39,54 @@ class SettingsDialog extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 26),
             child: SingleChildScrollView(
-              child: Consumer<SettingsProvider>(
-                builder: (context, settingsProvider, child) {
-                  toggleTheme() => settingsProvider.toggleDarkMode();
-
-                  void changeLanguage(BuildContext context, Locale locale) {
-                    context.setLocale(locale);
-                  }
-
-                  void signOut(BuildContext context) {
-                    Navigator.pop(context);
-                    context.read<UserManagementProvider>().signOutUser();
-                  }
-
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SettingsItem(
-                        title: AppLocalizations.darkMode.tr(),
-                        trailing: Switch(
-                          value:
-                              settingsProvider.appThemeMode == ThemeMode.dark,
-                          onChanged: (value) => toggleTheme(),
-                        ),
-                        onTap: toggleTheme,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Consumer<SettingsProvider>(
+                      builder: (context, settingsProvider, chile) {
+                    return SettingsItem(
+                      title: AppLocalizations.darkMode.tr(),
+                      trailing: Switch(
+                        value: settingsProvider.appThemeMode == ThemeMode.dark,
+                        onChanged: (value) => toggleTheme(settingsProvider),
                       ),
-                      const Divider(),
-                      SettingsItem(
-                        title: AppLocalizations.appLanguage.tr(),
-                        trailing: SegmentedButton<Locale>(
-                          segments: [
-                            ButtonSegment(
-                              value: ConstLocales.english,
-                              label: Text(
-                                ConstLocales.english.languageCode.toUpperCase(),
-                              ),
-                            ),
-                            ButtonSegment(
-                              value: ConstLocales.arabic,
-                              label: Text(
-                                ConstLocales.arabic.languageCode.toUpperCase(),
-                              ),
-                            ),
-                          ],
-                          selected: {context.locale},
-                          onSelectionChanged: (locale) => changeLanguage(
-                            context,
-                            locale.first,
+                      onTap: () => toggleTheme(settingsProvider),
+                    );
+                  }),
+                  const Divider(),
+                  SettingsItem(
+                    title: AppLocalizations.appLanguage.tr(),
+                    trailing: SegmentedButton<Locale>(
+                      segments: [
+                        ButtonSegment(
+                          value: ConstLocales.english,
+                          label: Text(
+                            ConstLocales.english.languageCode.toUpperCase(),
                           ),
                         ),
+                        ButtonSegment(
+                          value: ConstLocales.arabic,
+                          label: Text(
+                            ConstLocales.arabic.languageCode.toUpperCase(),
+                          ),
+                        ),
+                      ],
+                      selected: {
+                        testMode ? ConstLocales.english : context.locale
+                      },
+                      onSelectionChanged: (locale) => changeLanguage(
+                        context,
+                        locale.first,
                       ),
-                      const Divider(),
-                      SettingsItem(
-                        title: AppLocalizations.signOut.tr(),
-                        trailing: const Icon(Icons.logout),
-                        onTap: () => signOut(context),
-                      ),
-                    ],
-                  );
-                },
+                    ),
+                  ),
+                  const Divider(),
+                  SettingsItem(
+                    title: AppLocalizations.signOut.tr(),
+                    trailing: const Icon(Icons.logout),
+                    onTap: () => signOut(context),
+                  ),
+                ],
               ),
             ),
           ),
